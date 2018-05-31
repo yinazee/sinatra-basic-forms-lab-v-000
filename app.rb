f8c2b13616215ada87633bcd705a1a47eab9b3be
Yina Zhu <yinagit@gmail.com>
2018-05-31 00:29:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -10,7 +10,8 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/' do
+  post '/puppy' do
     @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    erb :display_puppy
   end
 end
