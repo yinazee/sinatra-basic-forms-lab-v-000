ca9cd80503e108dca528db421f477c4d0ceb516f
Yina Zhu <yinagit@gmail.com>
2018-05-31 00:11:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/' do
+  post '/puppy' do
     @puppy = Puppy.new(params[:name], params[:breed], params[:age])
     erb :display_puppy
   end
